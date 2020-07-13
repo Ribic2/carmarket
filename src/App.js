3b330fd4f0773b8bfaa2cc8ec52930331a40be93
Vid Bukovec <vid.bukovec8361@gmail.com>
2020-07-13 17:26:40 +0200

    Basic layout of an app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Typography } from '@material-ui/core'
+
+//Componenets
+import Header from '../src/components/Header'
+import CSS from './app.css'
 
 function App() {
+  const headerStyle = {
+    backgroundColor: "#4F8A8B",
+    height: "115px",
+  }
+
+  const headerTitle = {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: "50px",
+    lineHeight: "75px",
+    textAlign: "center",
+    color: "#F4F6FF",
+    position: "relative",
+    top: "11px",
+  }
+
+  const contentStyle = {
+    backgroundColor: "#F4F6FF",
+    height: "85vh",
+    marginTop: "10px"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Grid container spacing={2}>
+          <Grid item xs={12} style={headerStyle} className = "header">
+            <Typography style={headerTitle} className="headerTitle">
+              Anketa
+            </Typography>
+          </Grid>
+
+          <Grid item style={contentStyle} xs={12}>
+            
+          </Grid>
+      </Grid>
+       
+    
   );
 }
 
